b0cc4f6a970106ee96b990580e42daa2060d64bc
Adam K <adam@Adams-MacBook-Pro.local>
2014-11-29 22:08:20 +0000

    make return submit image search

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,12 +2,20 @@ $(document).on('click', '#go-search', function(){
 	findImagesOnGoogle({keywords: $('#search-term').val(), container: '#search-results'})
 });
 
+$(document).on('keydown', '#search-term', function(e) {
+	if (e.keyCode == 13) 
+	{
+		$('#go-search').click();
+	}
+});
+
 $(document).on('click', '#search-results img', function() {
 	var url = $(this).data('url');
 	$("#workspace img").remove();
 	var img = $("<img>").attr('src', url);
 	$("#workspace").append(img);
 });
+
 
 $(document).on('input', '#text', function() {
 	$("#caption").text($(this).val());
